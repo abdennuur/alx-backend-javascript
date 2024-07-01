4ccd0367b4e9c8cad560dd380bbbd1f04b0618e5
abdennuur <abdnourkhatib@gmail.com>
2024-07-01 21:36:22 +0000

    10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,8 +2,8 @@ export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  map.forEach((val, key) => {
-    if (val === 1) {
+  map.forEach((value, key) => {
+    if (value === 1) {
       map.set(key, 100);
     }
   });
