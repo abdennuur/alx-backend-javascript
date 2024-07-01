5da9c0a9fa4a3e66cbc9a0d4f7b0007d45949162
abdennuur <abdnourkhatib@gmail.com>
2024-07-01 21:11:18 +0000

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
-    throw Error('Position outside range');
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
   }
-  const nwBuf = new ArrayBuffer(length);
-  const int8 = new Int8Array(nwBufr, 0, length);
-  int8.set([value], position);
-  return new DataView(nwBufr);
+
+  int8Array[position] = value;
+  return new DataView(buffer);
 }
