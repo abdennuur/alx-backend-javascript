db90e0ac15e9307037a227cbd022143e5bd52dd6
abdennuur <abdnourkhatib@gmail.com>
2024-07-01 21:21:25 +0000

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,25 +1,11 @@
-export default function updateStudentGradeByCity(
-  students,
-  city,
-  newGrades,
-) {
-  const fltrStudents = students.filter(
-    (student) => student.location === city,
-  );
-
-  const gradeStdnts = fltrStudents.map(
-    (student) => {
-      for (const gradeInfo of newGrades) {
-        if (student.id === gradeInfo.studentId) {
-          student.grade = gradeInfo.grade;
-        }
-      }
-      if (student.grade === undefined) {
-        student.grade = 'N/A';
-      }
-      return student;
-    },
-  );
-
-  return gradeStdnts;
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter(student => student.location === city)
+    .map(student => {
+      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A'
+      };
+    });
 }
